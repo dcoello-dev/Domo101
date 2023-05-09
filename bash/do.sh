5b274e88fd9e9999c229ec4bcfe3cad6a15c9d4b
David Coello Pulido <davidcoello@eprosima.com>
2023-05-09 21:54:51 +0200

    add do.sh
    Signed-off-by: David Coello Pulido <davidcoello@eprosima.com>

---
+++ bash/do.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "holi"                             # imprime por pantalla holi
+mkdir prueba                            # creo un directorio
+touch prueba/archivo1                   # crea archivo1
+echo "hola k ase" > prueba/archivo2     # crea archivo 2
+ls -la prueba                           # vemos lo que hay en el direcotrio
+cat prueba/archivo2                     # sacamos el contenido de archivo2
