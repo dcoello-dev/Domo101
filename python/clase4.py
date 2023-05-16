c8e6c3f4e4f8d6078e5ac0f508c80f79eadcbfb6
David Coello Pulido <davidcoello@eprosima.com>
2023-05-16 21:43:32 +0200

    Clase 4
    Signed-off-by: David Coello Pulido <davidcoello@eprosima.com>

---
+++ python/clase4.py
@@ -0,0 +1,96 @@
+# nombre = "David"
+
+# tipo = type(nombre)
+
+# valor_de_print = print(tipo)
+
+# print(valor_de_print)
+
+# aqui creo las variables
+# numero_str = "1"
+# numero_int = 1
+
+# print(type(numero_str))
+# print(type(numero_int))
+
+# print(numero_int + numero_int)
+# print(numero_str + numero_str)
+
+# # casteo es convertir de un tipo a otro
+# print(numero_int + int(numero_str))
+# print(str(numero_int) + numero_str)
+
+# nombre = "david"
+# print(len("david")) # hardcodear
+
+# print(type("david"))
+
+# temperatura = 80
+
+# mensaje_de_salida_temperatura = "La temperatura es: "
+
+# print(mensaje_de_salida_temperatura + str(temperatura))
+
+# temperatura = 80
+
+# num_cilindros = 3
+
+# print(int(temperatura / num_cilindros))
+
+# tipado dinamico
+# numero = 1
+# print(numero)
+# numero = "david"
+# print(numero)
+
+# numero = 1
+# print(numero)
+
+# numero = numero + 1
+# print(numero)
+
+# entero = 1
+# decimal = 1.0
+# booleano = True
+# string = "lkjshdflkjsdf"
+
+# n_uno = 1
+# n_dos = 1
+
+# resultado = sumar(n_uno, n_dos)
+# resultado = sumar(resultado, n_dos)
+# resultado = mas_uno(resultado)
+
+# sumar(n_uno, "numero_dos")
+
+
+# print(resultado)
+
+def mas_uno(antes):
+    return antes + 1
+
+def sumar(numero_uno, numero_dos):
+    return numero_uno + numero_dos
+
+def imprimir(mensaje):
+    print(str(mensaje) + " hola k ase")
+
+temperatura = 100
+# imprimir(n_uno)
+nombre = "acenha"
+# not
+# and
+# or
+# if temperatura > 80 and temperatura < 110:
+# if (nombre == "David") or (len(nombre) > 10):
+#     print("el mas wapo")
+# elif nombre == "acenha":
+#     print(nombre)
+# elif nombre == "acenha":
+#     print(nombre)
+# elif nombre == "acenha":
+#     print(nombre)
+# else:
+#     print("no es")
+
+# print("despues")
